c436d54600c73cbd6e4fdd37e2b8f5547087eace
DuH Nunes <duhnunes.dev@gmail.com>
2025-09-01 18:05:29 -0300

    fix: gh-page drag img

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 export function App() {
   return (
-    <div className="bg-gray-950 text-slate-100 h-screen">
+    <div className="bg-gray-950 text-slate-100 h-screen  select-none">
       <section className="flex items-center justify-center h-full">
         <img
           src="/logo-light.png"
           alt="logo lexibase Enterprise mother"
-          className="size-54 hover:scale-105 transition-[scale, rotate] duration-1000 hover:rotate-1 select-none"
+          className="size-54 hover:scale-105 transition-[scale, rotate] duration-1000 hover:rotate-1"
         />
       </section>
     </div>
